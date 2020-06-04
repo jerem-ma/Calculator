70a032c792cd03d7d164e981e81f4e78ac754a01
Jérémy M <jeremy.maia@protonmail.com>
2020-06-04 11:17:03 +0200

    Initial commit

---
+++ app/src/testRelease/java/test.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class test{
+	
+	@Test
+	public final void testFalse(){
+		fail("C'est faux t'as vu");
+	}
+}
