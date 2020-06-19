4f2980ac93075a16fb83bf5efc49528b8c7d083d
Jérémy M <jeremy.maia@protonmail.com>
2020-06-19 18:47:29 +0200

    Add getMathematicalType method

--- app/src/main/java/fr/alpha/calculator/OperationManager.java
+++ app/src/main/java/fr/alpha/calculator/OperationManager.java
@@ -1,9 +1,15 @@
 package fr.alpha.calculator;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import android.content.res.Resources;
 import androidx.annotation.NonNull;
 import org.apache.commons.lang3.Validate;
 
 import fr.alpha.calculator.interfaces.IOperationManager;
+import fr.alpha.calculator.MathematicalType;
+import fr.alpha.calculator.R;
 
 public class OperationManager implements IOperationManager{
 
@@ -39,4 +45,24 @@ public class OperationManager implements IOperationManager{
 		Validate.notNull(operation);
 		this.operation = operation;
 	}
+
+	private MathematicalType getMathematicalType(char c){
+		if (Character.isDigit(c))
+			return MathematicalType.DIGIT;
+
+		final String cString = String.valueOf(c);
+		final boolean isDot = Pattern.matches("\\.", cString);
+		if (isDot)
+			return MathematicalType.DOT;
+
+		final Resources res = this.mainActivity.forwardResources();
+		final String[] signMath = res.getStringArray(R.array.sign_math);
+
+		final boolean isSignMath = Arrays.asList(signMath).contains(cString);
+		if (isSignMath)
+			return MathematicalType.SIGN;
+
+		return MathematicalType.NONE;
+	}
+
 }
