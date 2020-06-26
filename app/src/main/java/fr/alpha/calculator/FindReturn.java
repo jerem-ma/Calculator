a18cc8fb49d721e09dd7d9f053cae6e0125dfdda
Jérémy M <jeremy.maia@protonmail.com>
2020-06-26 15:34:41 +0200

    Add empty constructor for FindReturn

--- app/src/main/java/fr/alpha/calculator/FindReturn.java
+++ app/src/main/java/fr/alpha/calculator/FindReturn.java
@@ -5,6 +5,11 @@ public class FindReturn{
 	private final int index;
 	private final char sign;
 
+	public FindReturn() {
+		this.index = -1;
+		this.sign = '\0';
+	}
+
 	public FindReturn(final int index, final char sign){
 		this.index = index;
 		this.sign = sign;
