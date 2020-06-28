215857589327d256431a1929d054ab14080f347c
Jérémy M <jeremy.maia@protonmail.com>
2020-06-28 09:19:41 +0200

    Create method for onClick of buttons

--- app/src/main/java/fr/alpha/calculator/MainActivity.java
+++ app/src/main/java/fr/alpha/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package fr.alpha.calculator;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 
 import fr.alpha.calculator.R;
 
@@ -25,4 +26,86 @@ public class MainActivity extends Activity {
 	public Resources forwardResources(){
 		return getResources();
 	}
+
+	/* ** Events ** */
+		// Numbers
+	public void zeroButtonClick(View view){
+		
+	}
+
+	public void oneButtonClick(View view){
+
+	}
+
+	public void twoButtonClick(View view){
+
+	}
+
+	public void threeButtonClick(View view){
+
+	}
+
+	public void fourButtonClick(View view){
+
+	}
+
+	public void fiveButtonClick(View view){
+
+	}
+
+	public void sixButtonClick(View view){
+
+	}
+
+	public void sevenButtonClick(View view){
+
+	}
+
+	public void eightButtonClick(View view){
+
+	}
+
+	public void nineButtonClick(View view){
+
+	}
+
+		// Math signs
+	public void sumButtonClick(View view){
+
+	}
+
+	public void minusButtonClick(View view){
+
+	}
+
+	public void multiplyButtonClick(View view){
+
+	}
+
+	public void divideButtonClick(View view){
+
+	}
+
+	public void percentButtonClick(View view){
+
+	}
+
+		// Delete buttons
+	public void deleteButtonClick(View view){
+
+	}
+
+	public void clearButtonClick(View view){
+
+	}
+
+		// Divers
+	public void decimalPointButtonClick(View view){
+
+	}
+
+	public void resultButtonClick(View view){
+
+	}
+
 }
