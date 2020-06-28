d14e411cde7ec06f528635cfaf6867e9be5b0fb3
Jérémy M <jeremy.maia@protonmail.com>
2020-06-28 09:19:07 +0200

    Initialize the manager in the activity

--- app/src/main/java/fr/alpha/calculator/MainActivity.java
+++ app/src/main/java/fr/alpha/calculator/MainActivity.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 
+import fr.alpha.calculator.R;
+
 public class MainActivity extends Activity {
+
+	private OperationManager manager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final Resources res = getResources();
+		final String[] mathSigns = res.getStringArray(R.array.sign_math);
+		manager = new OperationManager(mathSigns);
 	}
 
 	/**
