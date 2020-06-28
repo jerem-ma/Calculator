0134e484b470a4ee707bd902fb341dc1fcc3f9dd
Jérémy M <jeremy.maia@protonmail.com>
2020-06-28 09:28:25 +0200

    Link button and OperationManager

--- app/src/main/java/fr/alpha/calculator/MainActivity.java
+++ app/src/main/java/fr/alpha/calculator/MainActivity.java
@@ -30,82 +30,82 @@ public class MainActivity extends Activity {
 	/* ** Events ** */
 		// Numbers
 	public void zeroButtonClick(View view){
-		
+		manager.addCharacter('0');
 	}
 
 	public void oneButtonClick(View view){
-
+		manager.addCharacter('1');
 	}
 
 	public void twoButtonClick(View view){
-
+		manager.addCharacter('2');
 	}
 
 	public void threeButtonClick(View view){
-
+		manager.addCharacter('3');
 	}
 
 	public void fourButtonClick(View view){
-
+		manager.addCharacter('4');
 	}
 
 	public void fiveButtonClick(View view){
-
+		manager.addCharacter('5');
 	}
 
 	public void sixButtonClick(View view){
-
+		manager.addCharacter('6');
 	}
 
 	public void sevenButtonClick(View view){
-
+		manager.addCharacter('7');
 	}
 
 	public void eightButtonClick(View view){
-
+		manager.addCharacter('8');
 	}
 
 	public void nineButtonClick(View view){
-
+		manager.addCharacter('9');
 	}
 
 		// Math signs
 	public void sumButtonClick(View view){
-
+		manager.addCharacter('+');
 	}
 
 	public void minusButtonClick(View view){
-
+		manager.addCharacter('-');
 	}
 
 	public void multiplyButtonClick(View view){
-
+		manager.addCharacter('*');
 	}
 
 	public void divideButtonClick(View view){
-
+		manager.addCharacter('/');
 	}
 
 	public void percentButtonClick(View view){
-
+		// One day maybe, but not for this version
 	}
 
 		// Delete buttons
 	public void deleteButtonClick(View view){
-
+		manager.removeCharacter();
 	}
 
 	public void clearButtonClick(View view){
-
+		manager.clearOperation();
 	}
 
 		// Divers
 	public void decimalPointButtonClick(View view){
-
+		manager.addCharacter('.');
 	}
 
 	public void resultButtonClick(View view){
-
+		final double result = manager.computes();
 	}
 
 }
