2807d53cc2a475f75e4d04568f412a506b5e3d73
Jérémy M <jeremy.maia@protonmail.com>
2020-07-01 13:41:33 +0200

    Get result widget

--- app/src/main/java/fr/alpha/calculator/MainActivity.java
+++ app/src/main/java/fr/alpha/calculator/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import fr.alpha.calculator.R;
 
 public class MainActivity extends Activity {
 
 	private OperationManager manager;
+	private TextView result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends Activity {
 		final Resources res = getResources();
 		final String[] mathSigns = res.getStringArray(R.array.sign_math);
 		manager = new OperationManager(mathSigns);
+		result = (TextView) findViewById(R.id.result);
 	}
 
 	/**
